b653d25a994188f2e6770417ffb24e2d01a125a0
임빛나 <devbit4@gmail.com>
2022-04-19 02:59:42 +0900

    styled컴포넌트 애니메이션/pseudo 선택자

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+
+const rotation = keyframes`
+0% {
+transform: rotate(0deg);
+border-radius: 0px;
+}
+50%{
+transform: rotate(360deg);
+border-radius: 100px;
+}
+100%{
+transform: rotate(0deg);
+border-radius: 0px;
+}
 `;
-const Link = styled(Btn)``;
-// 속성 추가
-const Input = styled.input.attrs({ required: true })`
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  display: flex;
   background-color: tomato;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotation} 10s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+    &:active {
+      opacity: 0;
+      // 클릭하면 active 상태
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as='header'>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😤</span>
+      </Box>
+    </Wrapper>
   );
 }
 
