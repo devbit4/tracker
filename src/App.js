bc232af4ecbd701bbc2f5584a31b33a989d3a6b9
임빛나 <devbit4@gmail.com>
2022-04-19 02:46:29 +0900

    styled컴포넌트 적용 test2

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import styled from 'styled-components';
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Link = styled(Btn)``;
+// 속성 추가
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor='teal' />
-      <Circle bgColor='tomato' />
+    <Father as='header'>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
